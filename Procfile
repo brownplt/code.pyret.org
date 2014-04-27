1b50da3e0a92e7d443e203c59a2ca49a6bdd6c81
Joe Gibbs Politz <joe@cs.brown.edu>
2014-04-27 19:19:41 -0400

    set up for static files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:  node src/scripts/static-files.js
---
+++ src/scripts/static-files.js
@@ -0,0 +1,27 @@
+var static = require('node-static'),
+  http = require('http'),
+  util = require('util');
+
+var webroot = './node_modules/pyret-lang/',
+  port = 8080;
+
+var file = new(static.Server)(webroot, { 
+  cache: 600, 
+  headers: { 'X-Powered-By': 'node-static' } 
+});
+
+http.createServer(function(req, res) {
+  console.log("Request received");
+  file.serve(req, res, function(err, result) {
+    console.log("Done serving");
+    if (err) {
+      console.error('Error serving %s - %s', req.url, err.message);
+      res.writeHead(err.status, err.headers);
+      res.end();
+    } else {
+      console.log('%s - %s', req.url, res.message); 
+    }
+  });
+}).listen(port);
+
+console.log('node-static running at http://localhost:%d', port);
