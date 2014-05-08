12d7ab518724644b34b7092a1774eb9bc7598c8a
Joe Gibbs Politz <joe@cs.brown.edu>
2014-05-08 10:12:45 -0400

    put this into a shared library

--- test/browser/signin.js
+++ test/browser/signin.js
@@ -1,117 +1,64 @@
 _ = require("jasmine-node");
-var server = require("./../../src/server.js");
+var tester = require("./selenium-init.js");
 var webdriver = require('selenium-webdriver');
 
-if (process.env["TEST_LOC"] === "local") {
-  server.start({
-    baseUrl: process.env["BASE_URL"],
-    port: process.env["PORT"],
-    sessionSecret: process.env["SESSION_SECRET"],
-    google: {
-      clientId: process.env["GOOGLE_CLIENT_ID"],
-      clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
-      redirect: "/oauth2callback"
-    }
-  }, function(app, server) {
-    var driver = new webdriver.Builder().
-      withCapabilities({browserName: "chrome"}).
-      build();
-    afterServer(server, process.env["BASE_URL"], driver);
-  });
-}
-else {
-  var driver = new webdriver.Builder().
-    usingServer("https://ondemand.saucelabs.com/wd/hub").
-    withCapabilities({
-      browserName: "internet explorer",
-      username: process.env["SAUCE_USERNAME"],
-      accessKey: process.env["SAUCE_ACCESS_KEY"]
-    }).
-    build();
-  afterServer(null, process.env["SAUCE_TEST_TARGET"], driver);
-}
-  
+var contains = tester.contains;
+var waitThenClick = tester.waitThenClick;
+var googleLogin = tester.googleLogin;
+var googleLogout = tester.googleLogout;
 
-var googleUsername = process.env["SELENIUM_GOOGLE_USER"];
-var googlePassword = process.env["SELENIUM_GOOGLE_PASSWORD"];
-
-function afterServer(maybeServer, baseUrl, driver) {
-  function googleLogin(driver) {
-    driver.wait(function() {
-      return driver.getTitle().then(function(title) {
-        return title === 'Sign in - Google Accounts';
-      });
-    }, 3000);
-    // Sometimes email isn't present because the browser remembers which
-    // Google account we last logged in as
-    driver.findElement(webdriver.By.id("Email")).getAttribute("class").then(function(cls) {
-      if(cls.indexOf("hidden") === -1) {
-        driver.findElement(webdriver.By.id("Email")).sendKeys(googleUsername);
-      }
-      driver.findElement(webdriver.By.id("Passwd")).sendKeys(googlePassword);
-      driver.findElement(webdriver.By.id("signIn")).click();
-    });
-  }
-  function waitThenClick(driver, query) {
-    driver.wait(function() {
-      return driver.isElementPresent(query);
-    }, 4000);
-    return driver.findElement(query).click();
-  }
+tester.start(function(maybeServer, baseUrl, driver) {
   describe("Sign in", function() {
-    webdriver.promise.controlFlow().on('uncaughtException', function(e) {
-      console.error('Unhandled error: ' + e);
-      fail();
-    });
 
-    it("Should forget everything it knows", function(done) {
-      try {
-        driver.get("https://security.google.com/settings/security/permissions");
-        googleLogin(driver);
-        console.log("waiting...");
-        driver.wait(function() {
-          console.log("Waiting for page...");
-          return driver.executeScript("return document.readyState === 'complete'");
-        }, 3000);
-//        driver.isElementPresent(webdriver.By.xpath("//*[contains(text(), 'Patch Test')]")).then(function(p) { console.log(p); });
-          driver.isElementPresent(webdriver.By.xpath("//*[contains(text(), 'Patch Test')]")).then(function(present) {
-            console.log("present: ", present);
-            if(present) {
-              waitThenClick(driver, webdriver.By.xpath("//*[contains(text(), 'Patch Test')]"));
-              waitThenClick(driver, webdriver.By.xpath("//*[contains(text(), 'Revoke access')]"));
-              return waitThenClick(driver, webdriver.By.name("ok"));
-            } else {
-              // do nothing
-            }
-          });
-        driver.get("https://accounts.google.com/Logout");
-        driver.call(function() { done(); });
-      }
-      catch(e) { console.log(e); }
+    tester.webbit("Should forget everything it knows", function(done) {
+      driver.get("https://security.google.com/settings/security/permissions");
+      googleLogin(driver);
+      console.log("Waiting to see if Patch permissions are present...");
+      driver.wait(function() {
+        return driver.executeScript("return document.readyState === 'complete'");
+      }, 3000);
+      driver.isElementPresent(contains("Patch Test")).then(function(present) {
+        if(present) {
+          waitThenClick(driver, contains("Patch Test"));
+          waitThenClick(driver, contains("Revoke access"));
+          return waitThenClick(driver, webdriver.By.name("ok"));
+        } else {
+          // do nothing otherwise
+        }
+      });
+      googleLogout(driver);
+      driver.call(done);
     }, 60000);
 
-    it("Should sign up from not being logged in", function(done) {
-      console.log("sign up");
+    tester.webbit("Should sign up from not being logged in", function(done) {
       driver.get(baseUrl);
       driver.findElement(webdriver.By.id('login')).click();
       googleLogin(driver);
+      console.log("Waiting for permission confirmation button...");
       driver.wait(function() {
-        console.log("Waiting for confirmation button to activate...");
         return driver.findElement(webdriver.By.id("submit_approve_access")).getAttribute("disabled")
         .then(function(disabled) {
           return !disabled;
         });
       }, 3000);
       driver.findElement(webdriver.By.id("submit_approve_access")).click();
-      var loaded = driver.wait(function() {
-        console.log("Waiting for page to load..");
-        return driver.findElement(webdriver.By.id("loader")).getCssValue("display")
-        .then(function(d) {
-          console.log("style: ", d);
-          return d === "none";
-        });
-      }, 3000);
-      loaded.then(function() { console.log("done with test"); done(); });
+      tester.waitForPyretLoad(driver);
+      driver.call(done);
+    }, 60000);
+
+    tester.webbit("When logging back in, should skip authentication and go straight to my-programs", function(done) {
+      driver.get(baseUrl);
+      driver.findElement(webdriver.By.id('login')).click();
+      tester.waitForPyretLoad(driver);
+      driver.call(done);
+    }, 60000);
+
+    tester.webbit("If cookies are cleared, should still log in seamlessly and work", function(done) {
+      driver.manage().deleteAllCookies();
+      driver.get(baseUrl);
+      driver.findElement(webdriver.By.id('login')).click();
+      tester.waitForPyretLoad(driver);
+      driver.call(done);
     }, 60000);
 
     it("Should close the server and the connection to the browser", function(done) {
@@ -122,4 +69,4 @@ function afterServer(maybeServer, baseUrl, driver) {
     });
 
   });
-}
+});
