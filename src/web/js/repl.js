bfc330c73721ba24b5e9e1135163bb01f4453789
Joe Gibbs Politz <joe@cs.brown.edu>
2014-06-12 21:39:37 -0400

    Revert "guess gas"
    This reverts commit 89468fba667fe8749bdb7ae67966b948ebb8dbad.

--- src/web/js/repl.js
+++ src/web/js/repl.js
@@ -18,7 +18,7 @@ $(function() {
   if(params.get["share"] && !shownBefore) {
     window.localStorage.setItem(key, true);
     var explanation = $("<div>").append([$("<p>")
-      .text("You're viewing a copy of a shared program.  " +
+      .text("You're viewing a copy of a shared program." +
             "Feel free to edit it: don't worry, you won't change the original!"),
       $("<p>").text(
             "You can save a copy of the program to " +
@@ -38,7 +38,7 @@ $(function() {
 
 $(function() {
   var BASE = "http://cs.brown.edu/~joe/private/pyret-dev/";
-  define("repl-main", ["js/repl-lib", "/js/repl-ui.js", "js/runtime-anf", "js/dialects-lib", "/js/guess-gas.js"], function(replLib, replUI, rtLib, dialectLib, guessGas) {
+  define("repl-main", ["js/repl-lib", "/js/repl-ui.js", "js/runtime-anf", "js/dialects-lib"], function(replLib, replUI, rtLib, dialectLib) {
     makeHoverMenu($("#menu"), $("#menuContents"), function() {});
     var replContainer = $("<div>").addClass("repl");
     $("#REPL").append(replContainer);
@@ -60,221 +60,206 @@ $(function() {
       runtime.safeCall(function() {
         return replLib.create(runtime, replNS, replEnv, {name: "definitions", dialect: dialectStr});
       }, function(repl) {
-        var gassed = guessGas.guessGas(10000, repl);
-        gassed.fail(function(err) {
-          console.error("Couldn't guess gas: ", err);
+
+        // NOTE(joe): This forces the loading of all the built-in compiler libs
+        var interactionsReady = repl.restartInteractions("");
+        var runButton = $("#runButton");
+
+        var replWidget = replUI.makeRepl(replContainer, repl, runtime, {
+            breakButton: $("#breakButton"),
+            runButton: runButton
+          });
+        window.RUN_CODE = function(src, uiOpts, replOpts) {
+          replWidget.runCode(src, uiOpts, replOpts);
+        };
+        var codeContainer = $("<div>").addClass("replMain");
+        $("#main").prepend(codeContainer);
+        var editor = replUI.makeEditor(codeContainer, {
+            runButton: $("#runButton"),
+            simpleEditor: false,
+            initial: "print('Ahoy, world!')",
+            run: RUN_CODE,
+            initialGas: 500
+          });
+        $(window).on("keyup", function(e) {
+          if(e.keyCode === 27) { // "ESC"
+            $("#help-keys").fadeOut(500);
+            e.stopImmediatePropagation();
+            e.preventDefault();
+          }
         });
-        var done = gassed.then(function(repl) {
-          console.log("Gas succeeded at: ", repl.runtime.INITIAL_GAS);
-
-          // NOTE(joe): This forces the loading of all the built-in compiler libs
-          var interactionsReady = repl.restartInteractions("");
-          interactionsReady.fail(function(err) {
-            console.error("Couldn't start REPL: ", err);
+        function autoSave() {
+          programToSave.then(function(p) {
+            if(p !== null && !copyOnSave) { save(); }
           });
-          interactionsReady.then(function(result) {
-            console.error("REPL ought to be red eye ", result);
-          });
-          var runButton = $("#runButton");
-
-          var replWidget = replUI.makeRepl(replContainer, repl, runtime, {
-              breakButton: $("#breakButton"),
-              runButton: runButton
-            });
-          window.RUN_CODE = function(src, uiOpts, replOpts) {
-            replWidget.runCode(src, uiOpts, replOpts);
-          };
-          var codeContainer = $("<div>").addClass("replMain");
-          $("#main").prepend(codeContainer);
-          var editor = replUI.makeEditor(codeContainer, {
-              runButton: $("#runButton"),
-              simpleEditor: false,
-              initial: "print('Ahoy, world!')",
-              run: RUN_CODE,
-              initialGas: 100
-            });
-          $(window).on("keyup", function(e) {
-            if(e.keyCode === 27) { // "ESC"
-              $("#help-keys").fadeOut(500);
+        }
+        $(window).on("keydown", function(e) {
+          if(e.ctrlKey) {
+            if(e.keyCode === 83) { // "Ctrl-s"
+              save();
               e.stopImmediatePropagation();
               e.preventDefault();
             }
-          });
-          function autoSave() {
-            programToSave.then(function(p) {
-              if(p !== null && !copyOnSave) { save(); }
-            });
-          }
-          $(window).on("keydown", function(e) {
-            if(e.ctrlKey) {
-              if(e.keyCode === 83) { // "Ctrl-s"
-                save();
-                e.stopImmediatePropagation();
-                e.preventDefault();
-              }
-              else if(e.keyCode === 13) { // "Ctrl-Enter"
-                editor.run();
-                autoSave();
-                e.stopImmediatePropagation();
-                e.preventDefault();
-              } else if(e.keyCode === 68) { // "Ctrl-d"
-                editor.focus();
-                e.stopImmediatePropagation();
-                e.preventDefault();
-              } else if(e.keyCode === 73) { // "Ctrl-i"
-                replWidget.focus();
-                e.stopImmediatePropagation();
-                e.preventDefault();
-              } else if(e.keyCode === 191 && e.shiftKey) { // "Ctrl-?"
-                $("#help-keys").fadeIn(100);
-                e.stopImmediatePropagation();
-                e.preventDefault();
-              }
+            else if(e.keyCode === 13) { // "Ctrl-Enter"
+              editor.run();
+              autoSave();
+              e.stopImmediatePropagation();
+              e.preventDefault();
+            } else if(e.keyCode === 68) { // "Ctrl-d"
+              editor.focus();
+              e.stopImmediatePropagation();
+              e.preventDefault();
+            } else if(e.keyCode === 73) { // "Ctrl-i"
+              replWidget.focus();
+              e.stopImmediatePropagation();
+              e.preventDefault();
+            } else if(e.keyCode === 191 && e.shiftKey) { // "Ctrl-?"
+              $("#help-keys").fadeIn(100);
+              e.stopImmediatePropagation();
+              e.preventDefault();
             }
-          });
-          
+          }
+        });
+        
 
+        storageAPI.then(function(api) {
+          api.collection.then(function() {
+            $(".loginOnly").show();
+            $(".logoutOnly").hide();
+          });
+          api.collection.fail(function() {
+            $(".loginOnly").hide();
+            $(".logoutOnly").show();
+          });
+        });
+
+        storageAPI = storageAPI.then(function(api) { return api.api; });
+        $("#connectButton").click(function() {
+          $("#connectButton").text("Connecting...");
+          $("#connectButton").attr("disabled", "disabled");
+          storageAPI = createProgramCollectionAPI(clientId, apiKey, "code.pyret.org", false);
           storageAPI.then(function(api) {
-            api.collection.then(function() {
+            api.collection.then(function() {    
               $(".loginOnly").show();
               $(".logoutOnly").hide();
+              if(params["get"] && params["get"]["program"]) {
+                var toLoad = api.api.getFileById(params["get"]["program"]);
+                console.log("Logged in and has program to load: ", toLoad);
+                loadProgram(toLoad);
+                programToSave = toLoad;
+              } else {
+                programToSave = Q.fcall(function() { return null; });
+              }
             });
-            api.collection.fail(function() {
-              $(".loginOnly").hide();
-              $(".logoutOnly").show();
+            api.collection.fail(function(err) {
+              $("#connectButton").text("Connect to Google Drive");
+              $("#connectButton").attr("disabled", false);
             });
           });
-
           storageAPI = storageAPI.then(function(api) { return api.api; });
-          $("#connectButton").click(function() {
-            $("#connectButton").text("Connecting...");
-            $("#connectButton").attr("disabled", "disabled");
-            storageAPI = createProgramCollectionAPI(clientId, apiKey, "code.pyret.org", false);
-            storageAPI.then(function(api) {
-              api.collection.then(function() {    
-                $(".loginOnly").show();
-                $(".logoutOnly").hide();
-                if(params["get"] && params["get"]["program"]) {
-                  var toLoad = api.api.getFileById(params["get"]["program"]);
-                  console.log("Logged in and has program to load: ", toLoad);
-                  loadProgram(toLoad);
-                  programToSave = toLoad;
-                } else {
-                  programToSave = Q.fcall(function() { return null; });
-                }
-              });
-              api.collection.fail(function(err) {
-                $("#connectButton").text("Connect to Google Drive");
-                $("#connectButton").attr("disabled", false);
-              });
-            });
-            storageAPI = storageAPI.then(function(api) { return api.api; });
-          });
+        });
 
-          var copyOnSave = false;
+        var copyOnSave = false;
 
-          var initialProgram = storageAPI.then(function(api) {
-            var programLoad = null;
-            if(params["get"] && params["get"]["program"]) {
-              programLoad = api.getFileById(params["get"]["program"]);
-              programLoad.then(function(p) { showShareContainer(p); });
-            }
-            if(params["get"] && params["get"]["share"]) {
-              programLoad = api.getSharedFileById(params["get"]["share"]);
-              $("#saveButton").text("Save a Copy");
-              copyOnSave = true;
-            }
-            if(programLoad) {
-              programLoad.fail(function(err) {
-                console.error(err);
-                stickError("The program failed to load.");
-              });
-              return programLoad;
-            } else {
-              return null;
-            }
-          });
-
-          function setTitle(progName) {
-            document.title = progName + " - code.pyret.org";
+        var initialProgram = storageAPI.then(function(api) {
+          var programLoad = null;
+          if(params["get"] && params["get"]["program"]) {
+            programLoad = api.getFileById(params["get"]["program"]);
+            programLoad.then(function(p) { showShareContainer(p); });
           }
+          if(params["get"] && params["get"]["share"]) {
+            programLoad = api.getSharedFileById(params["get"]["share"]);
+            $("#saveButton").text("Save a Copy");
+            copyOnSave = true;
+          }
+          if(programLoad) {
+            programLoad.fail(function(err) {
+              console.error(err);
+              stickError("The program failed to load.");
+            });
+            return programLoad;
+          } else {
+            return null;
+          }
+        });
 
-          function loadProgram(p) {
-            return p.then(function(p) {
-              if(p !== null) {
-                $("#program-name").val(p.getName());
-                setTitle(p.getName());
-                return p.getContents().then(function(c) {
-                  editor.cm.setValue(c);
+        function setTitle(progName) {
+          document.title = progName + " - code.pyret.org";
+        }
+
+        function loadProgram(p) {
+          return p.then(function(p) {
+            if(p !== null) {
+              $("#program-name").val(p.getName());
+              setTitle(p.getName());
+              return p.getContents().then(function(c) {
+                editor.cm.setValue(c);
+              });
+            }
+          });
+        }
+
+        var programLoaded = loadProgram(initialProgram); 
+
+        var programToSave = initialProgram;
+
+        function showShareContainer(p) {
+          $("#shareContainer").empty();
+          $("#shareContainer").append(makeShareLink(p));
+        }
+
+        function nameOrUntitled() {
+          return $("#program-name").val() || "Untitled";
+        }
+        function save() {
+          stickMessage("Saving...");
+          var savedProgram = programToSave.then(function(p) {
+            if(p !== null && !copyOnSave) {
+              if(p.getName() !== $("#program-name").val()) {
+                programToSave = p.rename(nameOrUntitled()).then(function(newP) {
+                  return newP;
                 });
               }
-            });
-          }
+              return programToSave
+              .then(function(p) {
+                showShareContainer(p);
+                return p.save(editor.cm.getValue(), false);
+              })
+              .then(function(p) {
+                $("#program-name").val(p.getName());
+                $("#saveButton").text("Save");
+                history.pushState(null, null, "#program=" + p.getUniqueId());
+                window.location.hash = "#program=" + p.getUniqueId();
+                flashMessage("Program saved as " + p.getName());
+                setTitle(p.getName());
+                return p;
+              })
+            }
+            else {
+              var programName = $("#program-name").val() || "Untitled";
+              $("#program-name").val(programName);
+              programToSave = storageAPI
+                .then(function(api) { return api.createFile(programName); });
+              copyOnSave = false;
+              return save();
+            }
+          });
+          savedProgram.fail(function(err) {
+            stickError("Unable to save", "Your internet connection may be down, or something else might be wrong with this site or saving to Google.  You should back up any changes to this program somewhere else.  You can try saving again to see if the problem was temporary, as well.");
+            console.error(err);
+          });
+        }
+        $("#runButton").click(autoSave);
+        
+        $("#saveButton").click(save);
 
-          var programLoaded = loadProgram(initialProgram); 
-
-          var programToSave = initialProgram;
-
-          function showShareContainer(p) {
-            $("#shareContainer").empty();
-            $("#shareContainer").append(makeShareLink(p));
-          }
-
-          function nameOrUntitled() {
-            return $("#program-name").val() || "Untitled";
-          }
-          function save() {
-            stickMessage("Saving...");
-            var savedProgram = programToSave.then(function(p) {
-              if(p !== null && !copyOnSave) {
-                if(p.getName() !== $("#program-name").val()) {
-                  programToSave = p.rename(nameOrUntitled()).then(function(newP) {
-                    return newP;
-                  });
-                }
-                return programToSave
-                .then(function(p) {
-                  showShareContainer(p);
-                  return p.save(editor.cm.getValue(), false);
-                })
-                .then(function(p) {
-                  $("#program-name").val(p.getName());
-                  $("#saveButton").text("Save");
-                  history.pushState(null, null, "#program=" + p.getUniqueId());
-                  window.location.hash = "#program=" + p.getUniqueId();
-                  flashMessage("Program saved as " + p.getName());
-                  setTitle(p.getName());
-                  return p;
-                })
-              }
-              else {
-                var programName = $("#program-name").val() || "Untitled";
-                $("#program-name").val(programName);
-                programToSave = storageAPI
-                  .then(function(api) { return api.createFile(programName); });
-                copyOnSave = false;
-                return save();
-              }
-            });
-            savedProgram.fail(function(err) {
-              stickError("Unable to save", "Your internet connection may be down, or something else might be wrong with this site or saving to Google.  You should back up any changes to this program somewhere else.  You can try saving again to see if the problem was temporary, as well.");
-              console.error(err);
-            });
-          }
-          $("#runButton").click(autoSave);
-          
-          $("#saveButton").click(save);
-
-          Q.all([programLoaded, interactionsReady]).fin(function() { $("#loader").hide(); });
-          editor.focus();
-        });
-        done.fail(function(err) {
-          console.error("Pyret failed to load.", err);
-        });
+        programLoaded.then(function() { $("#loader").hide(); });
+        Q.all([programLoaded, interactionsReady]).fin(function() { $("#loader").hide(); });
+        editor.focus();
       });
-    });
-    load.fail(function(err) {
-      console.error("Pyret failed to load.", err);
+      load.fail(function(err) {
+        console.error("Pyret failed to load.", err);
+      });
     });
   });
   require(["repl-main"]);
