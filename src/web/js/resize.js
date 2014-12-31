81f2a4d5f4efcb31b75d93b92227e0925fc833ec
Eli White <elibbw@gmail.com>
2014-12-31 14:13:08 -0900

    Changed Ctrl-m functionality
    toggles between 50% and last resized split

--- src/web/js/resize.js
+++ src/web/js/resize.js
@@ -1,5 +1,6 @@
 $( document ).ready( function() {
   $( "#REPL" ).css( "left", "50%");
+  var editorEvenSplit = true;
   var replHeight = $( "#REPL" ).height();
   $( "#REPL" ).resizable({
     maxHeight: replHeight,
@@ -11,6 +12,7 @@ $( document ).ready( function() {
   $( ".ui-resizable-handle").css("cursor", "ew-resize");
 
   $( "#REPL" ).on( "resize", leftResize);
+  $( "#REPL" ).on( "resize", function() {editorEvenSplit = false;});
 
   function leftResize(event, ui) {
   var leftWidth = (window.innerWidth - ui.size.width)
@@ -19,15 +21,45 @@ $( document ).ready( function() {
 
   $( "#REPL" ).on( "resizestop", toPercent);
 
+  var rightResizePct;
+  var leftResizePct;
   function toPercent(event, ui) {
     var winWidth = window.innerWidth
-    var rightWidthPct = (ui.size.width / winWidth) * 100
-    var leftWidthPct = 100 - rightWidthPct
-    $( "#REPL" ).css( "width", rightWidthPct + "%");
-    $( "#REPL" ).css( "left", leftWidthPct + "%");
-    $(".replMain").css("width", leftWidthPct + "%");
-  }});
+    rightResizePct = (ui.size.width / winWidth) * 100
+    leftResizePct = 100 - rightResizePct
+    setSize(leftResizePct, rightResizePct);
+  }
 
-$( window ).resize( function() {
+  $(window).on("keydown", function(e) {
+    if(e.ctrlKey) {
+      if(e.keyCode === 77) { // Ctrl-m
+        toggleEditorSize();
+        e.stopImmediatePropagation();
+        e.preventDefault();
+      }
+    }
+  })
+
+  function toggleEditorSize() {
+    if(editorEvenSplit) {
+      editorEvenSplit = false;
+      setSize(leftResizePct, rightResizePct);
+    }
+    else {
+      editorEvenSplit = true;
+      setSize("50", "50");
+    }
+    editor.refresh();
+  }
+
+
+  function setSize(leftPct, rightPct) {
+    $( "#REPL" ).css( "width", rightPct + "%");
+    $( "#REPL" ).css( "left", leftPct + "%");
+    $(".replMain").css("width", leftPct + "%");
+  }
+
+  $( window ).resize( function() {
     $( "#REPL" ).resizable( "option", "maxWidth", window.innerWidth - 128);
   });
+});
