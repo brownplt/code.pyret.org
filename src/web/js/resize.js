cb25d812e0b9b2029303986d403beea3849ed2bf
Eli White <elibbw@gmail.com>
2014-12-31 14:19:42 -0900

    change resize handle size, position
    make resize handle thicker, move down so as to not overlap header

--- src/web/js/resize.js
+++ src/web/js/resize.js
@@ -7,9 +7,15 @@ $( document ).ready( function() {
     maxWidth: window.innerWidth - 128,
     minHeight: replHeight,
     minWidth: 100,
-    handles: "w"});
+    handles: {"w": "#handle"}});
 
   $( ".ui-resizable-handle").css("cursor", "ew-resize");
+  $( ".ui-resizable-w" ).css("height", "calc(100% - 55px)");
+  $( ".ui-resizable-w" ).css("top", "55px");
+  $( ".ui-resizable-w" ).css("width", "5px");
+  $( ".ui-resizable-w" ).css("left", "0px");
+  $( ".ui-resizable-w" ).css("background", "black");
+  $( ".repl" ).css("padding-left", "10px");
 
   $( "#REPL" ).on( "resize", leftResize);
   $( "#REPL" ).on( "resize", function() {editorEvenSplit = false;});
