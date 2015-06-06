2aa97ab2a0b7463a59bb4c8f3dd239b23b217714
Joe Gibbs Politz <joe@cs.brown.edu>
2015-06-06 13:10:56 -0400

    unused share stuff

--- src/web/js/share.js
+++ src/web/js/share.js
@@ -1,53 +1,5 @@
 function makeShareAPI(pyretVersion) {
 
-  function drawShareWidget(shareUrl) {
-    var widget = $("<span class='share-buttons'>").append([
-        redditWidget(shareUrl),
-        googleWidget(shareUrl),
-        facebookWidget(shareUrl)
-      ]);
-    return widget;
-  }
-
-  function redditWidget(shareUrl) {
-    // NOTE(joe 19 May 2014):
-    // Adapted from first button at http://www.reddit.com/buttons/
-    var link = $("<a>");
-    link
-      .attr("href", "http://www.reddit.com/submit?url=" + encodeURIComponent(shareUrl))
-      .attr("target", "_blank")
-      .attr("alt", "Share on Reddit")
-      .append($("<img>")
-                .attr("src", "/img/spreddit1.gif")
-                .css("border", 0));
-    return link;
-  }
-
-  function googleWidget(shareUrl) {
-    var link = $("<a>");
-    link
-      .attr("href", "https://plus.google.com/share?url=" + encodeURIComponent(shareUrl))
-      .attr("target", "_blank")
-      .attr("alt", "Share on Google+")
-      .append($("<img>")
-                .attr({ "width": 14, "height": 14 })
-                .attr("src", "https://www.gstatic.com/images/icons/gplus-64.png")
-                .attr("alt", "Share on Google+"));
-    return link;
-  }
-
-  function facebookWidget(shareUrl) {
-    var link = $("<a>");
-    link
-      .attr("href", "https://facebook.com/sharer.php?u=" + encodeURIComponent(shareUrl))
-      .attr("target", "_blank")
-      .attr("alt", "Share on Facebook")
-      .append($("<img>")
-              .attr({ "width": 14, "height": 14 })
-              .attr("src", "http://www.wescheme.org/images/icon_facebook.gif"));
-    return link;
-  }
-
   function makeHoverMenu(triggerElt, menuElt, showOnHover, onShow) {
     var divHover = false;
     var linkHover = false;
@@ -193,7 +145,6 @@ function makeShareAPI(pyretVersion) {
   }
 
   return {
-    drawShareWidget: drawShareWidget,
     makeShareLink: makeShareLink,
     makeHoverMenu: makeHoverMenu,
     makeShareUrl: makeShareUrl
