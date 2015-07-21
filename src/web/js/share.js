b41c57fb0932c2be49426a8fb0a0aede4497607b
Joe Gibbs Politz <joe@cs.brown.edu>
2015-07-21 14:30:16 -0400

    tweaks to share workflow

--- src/web/js/share.js
+++ src/web/js/share.js
@@ -3,56 +3,52 @@ function makeShareAPI(pyretVersion) {
   function makeHoverMenu(triggerElt, menuElt, showOnHover, onShow) {
     var divHover = false;
     var linkHover = false;
+    var showing = false;
     function hovering() {
       return divHover || linkHover;
     }
     function closeIfNotHovering() {
       setTimeout(function() {
-        console.log(divHover, linkHover);
         if(!hovering()) {
           menuElt.fadeOut(500);
         }
       }, 500);
     }
-    function showIfStillHovering() {
-      setTimeout(function() {
-        if(linkHover) {
-          menuElt.css({
-            position: "fixed",
-            top: triggerElt.offset().top + triggerElt.outerHeight(),
-            left: triggerElt.offset().left,
-            "z-index": 12000
-          });
-          $(document.body).append(menuElt);
-          menuElt.fadeIn(250);
-          onShow();
-        }
-      }, 100);
+    function show() {
+      if(!showing) {
+        menuElt.css({
+          position: "fixed",
+          top: triggerElt.offset().top + triggerElt.outerHeight(),
+          left: triggerElt.offset().left,
+          "z-index": 10000
+        });
+        $(document.body).append(menuElt);
+        menuElt.fadeIn(250);
+        showing = true;
+        setTimeout(function() {
+          $(document).on("click", hide);
+        }, 0);
+        onShow();
+      }
     }
-    menuElt.hover(function() {
-      divHover = true;
-    }, function() {
-      divHover = false;
-      closeIfNotHovering();
+    var hide = function() {
+      showing = false;
+      menuElt.fadeOut(100);
+      $(document).off("click", hide);
+    };
+    menuElt.on("click", function(evt) {
+      evt.stopPropagation();
+      return false;
     });
-    triggerElt.click(function(e) {
-      linkHover = true;
-      showIfStillHovering();
-    });
-    triggerElt.hover(function(e) {
-      if(showOnHover) { showIfStillHovering(); }
-      linkHover = true;
-    }, function() {
-      linkHover = false;
-      closeIfNotHovering();
+    triggerElt.on("click", function(e) {
+      if(!showing) { show(); e.stopPropagation(); }
+      else { hide(); }
     });
     return triggerElt;
   }
 
   function makeShareLink(originalFile) {
     var link = $("<div>").append($("<button class=blueButton>").text("Share..."));
-    link.attr("title", "Create links to share with others, and see previous shared copies you've made.");
-    link.tooltip({ position: { my: "right top", of: link } });
     var shareDiv = $("<div>").addClass("share");
     return makeHoverMenu(link, shareDiv, false,
       function() {
@@ -68,8 +64,6 @@ function makeShareAPI(pyretVersion) {
       container.empty();
       console.log(sharedInstances);
       var a = $("<a>").text("Share a new copy").attr("href", "javascript:void(0)");
-      a.attr("title", "This will make a new copy of the file as you see it now, and create a link that you can share with others.  They will be able to see, run, and make their own copy of your program, but not edit the original.");
-      a.tooltip({ position: { my: "right top", of: a } });
       a.click(function() {
         var copy = originalFile.makeShareCopy();
         a.text("Copying...").attr("href", null);
@@ -122,29 +116,67 @@ function makeShareAPI(pyretVersion) {
   }
 
   function autoHighlightBox(text) {
-    var container = $("<div>");
     var textBox = $("<input type='text'>").addClass("auto-highlight");
-    container.append(textBox);
     textBox.attr("size", text.length);
     textBox.attr("editable", false);
-    textBox.mouseup(function() { $(this).select(); });
+    textBox.on("focus", function() { $(this).select(); });
+    textBox.on("mouseup", function() { $(this).select(); });
     textBox.val(text);
     return textBox;
   }
 
+  function getLanguage() {
+    if(typeof navigator !== "undefined") {
+      return navigator.language || "en-US"; // Biased towards USA
+    }
+    else {
+      return "en-US";
+    }
+  }
+
+  var dateOptions = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric"
+  };
+
   function drawShareRow(f) {
     var container = $("<div>").addClass("sharebox");
     var shareUrl = makeShareUrl(f.getUniqueId());
-    var showDate = new Date(f.getModifiedTime()).toLocaleString();
+    var displayDate = new Date(f.getModifiedTime()).toLocaleString(getLanguage, dateOptions);
     var hoverDate = String(new Date(f.getModifiedTime()));
-    container.append($("<label>").text(showDate).attr("alt", hoverDate));
-    container.append($("<br>"));
-    container.append(autoHighlightBox(shareUrl));
-    container.append($("<br>"));
+    container.append($("<label>").text(displayDate).attr("alt", hoverDate));
+    var shareLink = $("<a href='javascript:void()'>").text("(Share Link)").addClass("copy-link");
+    var importLink = $("<a href='javascript:void()'>").text("(Import Link)").addClass("copy-link");
+    container.append(shareLink);
+    container.append(importLink);
+    function showCopyText(title, text) {
+      var linkDiv = $("<div>").css({"z-index": 15000});
+      linkDiv.dialog({
+        title: title,
+        modal: true,
+			  overlay : { opacity: 0.5, background: 'black'},
+        width : "70%",
+        height : "auto",
+        closeOnEscape : true
+      });
+      var box = autoHighlightBox(text);
+      linkDiv.append(box);
+      box.focus();
+    }
+    shareLink.click(function() {
+      showCopyText("Copy Share Link", shareUrl);
+    });
+
     var importLetter = getImportLetter(f.getName()[0]);
     var importCode = "import shared-gdrive(\"" + f.getName() +
         "\", \"" + f.getUniqueId() + "\") as " + importLetter;
-    container.append(autoHighlightBox(importCode));
+    importLink.click(function() {
+      showCopyText("Copy Import Code", importCode);
+    });
     return container;
   }
 
