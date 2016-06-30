2c5c0abc2d5e25b9965af0154f74c06faf773d36
Paul Carduner <paul@carduner.net>
2016-06-30 16:50:37 -0700

    Update ide.js to use new api

--- src/web/js/ide.js
+++ src/web/js/ide.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import PyretIDE from 'pyret-ide';
-import ReactDOM from 'react-dom';
 import seedrandom from 'seedrandom';
 
 function loadScriptUrl(url) {
@@ -10,11 +8,45 @@ function loadScriptUrl(url) {
   document.body.appendChild(scriptTag);
 }
 
-// this is needed by pyret I guess.
-require('script!requirejs/require.js');
-window.define('seedrandom', [], function() { return seedrandom; });
-loadScriptUrl(process.env.BASE_URL+'/js/cpo-ide-hooks.jarr');
 
 var appDiv = document.createElement("div");
 document.body.appendChild(appDiv);
-ReactDOM.render(React.createElement(PyretIDE, null), appDiv);
+
+const runtimeApi = {
+  parse(src) {
+    return new Promise((resolve, reject) => reject(new Error("Not Implemented")));
+  },
+  compile(ast) {
+    return new Promise((resolve, reject) => reject(new Error("Not Implemented")));
+  },
+  execute(bytecode) {
+    return new Promise((resolve, reject) => reject(new Error("Not Implemented")));
+  },
+};
+
+PyretIDE.init({
+  rootEl: appDiv,
+  runtimeApiLoader() {
+    return new Promise((resolve, reject) => {
+
+      // this is needed by pyret I guess.
+      require('script!requirejs/require.js');
+      window.define('seedrandom', [], function() { return seedrandom; });
+      loadScriptUrl(process.env.BASE_URL+'/js/cpo-ide-hooks.jarr');
+
+      var startTime = new Date().getTime();
+      function checkIfLoaded() {
+        if (window.CPOIDEHooks) {
+          resolve(runtimeApi);
+        } else if (new Date().getTime() - startTime > 30000) {
+          reject(new Error("Timed out while waiting for runtime to load :("));
+        } else {
+          // ugh, we're polling to see if it's been loaded yet :(
+          window.setTimeout(checkIfLoaded, 250);
+        }
+      }
+
+      checkIfLoaded();
+    });
+  }
+});
