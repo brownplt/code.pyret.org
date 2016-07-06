2aeb27d66ddc2507b04312e38ff100e628e9affd
Joe Gibbs Politz <joe.politz@gmail.com>
2016-07-06 19:17:53 -0400

    Updates to make compile and run work.
    Note that this will require an npm update to get the newest horizon, and a
    rebuild of cpo-ide-hooks.jarr
    
    I put a bunch of TODOs and NOTEs in the code, which are intended as points of
    discussion.

--- src/web/js/cpo-ide-hooks.js
+++ src/web/js/cpo-ide-hooks.js
@@ -42,20 +42,76 @@
                       runtimeLib, loadLib, builtinModules, cpoBuiltins, parsePyret,
                       cpoModules
   ) {
+
+    var gf = runtime.getField;
+    var gmf = function(m, f) { return gf(gf(m, "values"), f); };
+
+    function findModule(contextIgnored, dependency) {
+      // TODO(joe): enhance this with gdrive locators, etc, later
+      return runtime.safeCall(function() {
+        return runtime.ffi.cases(gmf(compileStructs, "is-Dependency"), "Dependency", dependency,
+          {
+            builtin: function(name) {
+              var raw = cpoModules.getBuiltinLoadableName(runtime, name);
+              if(!raw) {
+                throw runtime.throwMessageException("Unknown module: " + name);
+              }
+              else {
+                return gmf(cpo, "make-builtin-js-locator").app(name, raw);
+              }
+            },
+            dependency: function(protocol, args) {
+              console.error("Unknown import: ", dependency);
+            }
+          });
+       }, function(l) {
+          return gmf(compileLib, "located").app(l, runtime.nothing);
+       }, "findModule");
+    }
+    var pyFindModule = runtime.makeFunction(findModule, "find-module");
+
+    // NOTE(joe): This line is "cheating" by mixing runtime levels, and uses
+    // the same runtime for the compiler and running code.  Usually you can
+    // only get a new pyret-viewable Runtime by calling create, but here we
+    // magic the current runtime into one.
+    // Someday Pyret will be quick enough that we won't need to save theses
+    // seconds of instantiation.
+    var pyRuntime = gf(gf(runtimeLib, "internal").brandRuntime, "brand").app(
+      runtime.makeObject({
+        "runtime": runtime.makeOpaque(runtime)
+      }));
+    var pyRealm = gf(loadLib, "internal").makeRealm(cpoModules.getRealm());
+
+    var builtins = [];
+    Object.keys(runtime.getParam("staticModules")).forEach(function(k) {
+      if(k.indexOf("builtin://") === 0) {
+        builtins.push(runtime.makeObject({
+          uri: k,
+          raw: cpoModules.getBuiltinLoadable(runtime, k)
+        }));
+      }
+    });
+    var builtinsForPyret = runtime.ffi.makeList(builtins);
+
+    function compile(ast) {
+      var compileAst = gmf(cpo, "compile-ast");
+      return runtime.safeTail(function() {
+        return compileAst.app(ast, pyRuntime, pyFindModule, gmf(compileStructs, "default-compile-options"));
+      });
+    }
+
+    function run(jsSrc) {
+      var run = gmf(cpo, "run");
+      return runtime.safeTail(function() {
+        return run.app(pyRuntime, pyRealm, jsSrc);
+      });
+    }
+
     window.CPOIDEHooks = {
       runtime: runtime,
-      namespace: namespace,
-      uri: uri,
-      compileLib: compileLib,
-      compileStructs: compileStructs,
-      pyRepl: pyRepl,
-      cpo: cpo,
-      runtimeLib: runtimeLib,
-      loadLib: loadLib,
-      builtinModules: builtinModules,
-      cpoBuiltins: cpoBuiltins,
       parsePyret: parsePyret,
-      cpoModules: cpoModules,
+      compile: compile,
+      run: run
     };
     return runtime.makeModuleReturn({}, {});
   }
--- src/web/js/ide.js
+++ src/web/js/ide.js
@@ -19,6 +19,23 @@ type AST: An instance of Program from src/arr/trove/ast.arr in pyret-lang
 */
 
 const runtimeApi = {
+  _getVal(mod, name) { // TODO(joe): is this an OK place to put helpers for this?
+    var runtime = window.CPOIDEHooks.runtime;
+    return runtime.getField(runtime.getField(window.CPOIDEHooks[mod], "values"), name);
+  },
+  _toReprOrDie(runtime, value, resolve, reject) {
+    runtime.runThunk(
+      () => runtime.toRepr(value),
+      (renderResult) => {
+        if(runtime.isSuccessResult(renderResult)) {
+          reject(renderResult.result);
+        }
+        else {
+          console.error("Could not render: ", value, " because ", renderResult);
+          reject("An error occurred while rendering a value, details logged to console");
+        }
+      });
+  },
   /*
     @param {string} src - some Pyret code
     @param {string} url - a unique name to store in source locations
@@ -31,24 +48,10 @@ const runtimeApi = {
     // TODO(joe): pass in a URL to uniquely identify this program
     if(!url) { url = "definitions://"; }
     var runtime = window.CPOIDEHooks.runtime;
-    function getVal(mod, name) {
-      return runtime.getField(runtime.getField(mod, "values"), name);
-    }
-    var parse = getVal(window.CPOIDEHooks.parsePyret, "surface-parse");
+    // TODO(joe): is "this" trustworthy here?
+    var self = this;
+    var parse = this._getVal("parsePyret", "surface-parse");
     return new Promise((resolve, reject) => {
-      function handleError(parseResult) {
-        runtime.runThunk(
-          () => runtime.toRepr(parseResult.exn.exn),
-          (exnResult) => {
-            if(runtime.isSuccessResult(exnResult)) {
-              reject(exnResult.result);
-            }
-            else {
-              console.error("Could not render: ", parseResult, " because ", exnResult);
-              reject("An error occurred while rendering a parse error, details logged to console");
-            }
-          });
-      }
       runtime.runThunk(
         () => parse.app(src, url),
         (parseResult) => {
@@ -56,7 +59,8 @@ const runtimeApi = {
             resolve(parseResult.result);
           }
           else {
-            handleError(parseResult);
+            // NOTE(joe): intentionally passing reject twice; want to report an error either way
+            self._toReprOrDie(runtime, parseResult.exn.exn, reject, reject);
           }
         });
     })
@@ -69,7 +73,30 @@ const runtimeApi = {
              (TODO: return richer values for error returns)
   */
   compile(ast) {
-    return new Promise((resolve, reject) => reject(new Error("Not Implemented")));
+    var runtime = window.CPOIDEHooks.runtime;
+    var get = runtime.getField;
+    var self = this;
+    return new Promise((resolve, reject) => {
+      runtime.runThunk(
+        () => window.CPOIDEHooks.compile(ast),
+        (compileResult) => {
+          // NOTE(joe): success here just means the compiler didn't blow up; the result
+          // is a Pyret Either indicating compile errors or a final JS program to run
+          if(runtime.isSuccessResult(compileResult)) {
+            var maybeJS = compileResult.result;
+            if(runtime.ffi.isLeft(maybeJS)) {
+              self._toReprOrDie(runtime, maybeJS, reject)
+            }
+            else {
+              resolve(get(maybeJS, "v"));
+            }
+          }
+          else {
+            // NOTE(joe): intentionally passing reject twice; want to report an error either way
+            self._toReprOrDie(runtime, compileResult.exn.exn, reject, reject);
+          }
+        });
+    });
   },
   /*
     @param {string} bytecode - JS code to evaluate
@@ -79,7 +106,28 @@ const runtimeApi = {
              (TODO: return richer values for error returns)
   */
   execute(bytecode) {
-    return new Promise((resolve, reject) => reject(new Error("Not Implemented")));
+    var runtime = window.CPOIDEHooks.runtime;
+    var get = runtime.getField;
+    var self = this;
+    return new Promise((resolve, reject) => {
+      runtime.runThunk(
+        () => window.CPOIDEHooks.run(bytecode),
+        (runResult) => {
+          // NOTE(joe): success here means the run succeeded, and will report
+          // both passing and failing tests, along with a final value
+
+          // Just doing a barebones dive to retrieve and return the toRepr of
+          // the final value for now, but there are lots of juicy things on
+          // this result, and it's something we should build out an API for.
+          var innerResult = runResult.result.val.result;
+          if(runtime.isSuccessResult(innerResult)) {
+            self._toReprOrDie(runtime, get(innerResult.result, "answer"), resolve, reject)
+          }
+          else {
+            self._toReprOrDie(runtime, innerResult.exn.exn, reject, reject);
+          }
+        });
+    });
   },
   stop() {
     
