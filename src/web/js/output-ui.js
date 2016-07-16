brownplt/code.pyret.org

commit dda7cc3dc74ba9e3e4d2bf9353e3383597797232

Fixing flash of blank-initial CM snippets, and also fixing unbound cmLoc error

src/web/js/output-ui.js
@@ -780,14 +780,36 @@
        || !!sessionStorage.getItem(cmloc.source)) {
         snippetWrapper.addClass("cm-snippet");
         var endch;
+        var source;
+        if (featured.source in editors) {
+          var cmsrc = editors[featured.source];
+          endch = cmsrc.getLine(cmloc.end.line).length;
+          source = cmsrc.getRange(
+            {line: cmloc.start.line, ch: 0},
+            {line: cmloc.end.line, ch: endch});
+        } else {
+          source = sessionStorage.getItem(cmloc.source)
+            .split("\n")
+            .slice(featured.start.line, featured.end.line + 1)
+            .join("\n");
+          endch = cmSnippet.getLine(cmSnippet.lastLine()).length;
+        }
+
+        var hack = $("<div>").addClass("repl"); // needed for proper CSS styling of the CM below
+        $(document.body).append(hack.append(snippetWrapper.css("position", "absolute").css("top", "-50000px")));
         var cmSnippet = CodeMirror(snippetWrapper[0],{
+          value: source,
           readOnly: "nocursor",
           disableInput: true,
           indentUnit: 2,
           lineWrapping: false,
           lineNumbers: true,
-          viewportMargin: 1,
+          viewportMargin: 0,
           scrollbarStyle: "null"});
+        cmSnippet.refresh(); // compute initial view
+        snippetWrapper.detach().css("position","").css("top", ""); // undo any CSS stupidity and remove
+        hack.remove(); // from document
+        
           
         if(cmloc.source in editors) {
           var cmsrc = editors[featured.source];
@@ -822,19 +844,8 @@
                 handle.on("clear", function(){cmsrc.off("change", refresh); });
               }
             });
-          // Copy relevant part of document.
-          endch = cmsrc.getLine(cmloc.end.line).length;
-          cmSnippet.getDoc().setValue(cmsrc.getRange(
-            {line: cmloc.start.line, ch: 0},
-            {line: cmloc.end.line, ch: endch}));
         } else {
-          var source = sessionStorage.getItem(cmloc.source)
-                      .split("\n")
-                      .slice(featured.start.line, featured.end.line + 1)
-                      .join("\n");
           cmSnippet.setOption("firstLineNumber", cmloc.start.line);
-          cmSnippet.getDoc().setValue(source);
-          endch = cmSnippet.getLine(cmSnippet.lastLine()).length;
         }
         // Fade areas outside featured range
         cmSnippet.getDoc().markText(

src/web/js/check-ui.js
@@ -133,7 +133,7 @@
                                type:"bookmark" });
             header.on("click", function(e){
               if(source === "definitions://") {
-                editors[source].scrollIntoView(cmLoc.start, 100);
+                editors[source].scrollIntoView(get(get(checkBlock, "loc"), "start-char"), 100);
               } else if (source.indexOf("interactions") != -1) {
                 editors[source].getWrapperElement().scrollIntoView(true);
               }
@@ -229,7 +229,7 @@
                              type:"bookmark" });
           name.on("click", function(e){
             if(source === "definitions://") {
-              editors[source].scrollIntoView(cmLoc.start, 100);
+              editors[source].scrollIntoView(get(get(checkBlock, "loc"), "start-char"), 100);
             } else if (source.indexOf("interactions") != -1) {
               editors[source].getWrapperElement().scrollIntoView(true);
             }

commit 01e2915fc68d2b54bc575020ca0a48fa23bd2933

Fixing bug introduced in dda7cc3dc74ba9e3e4d2bf9353e3383597797232: since cmSnippet isn't defined yet, we can't use it to compute the length of the final line of text.  This caused a dereference-undefined error that crashed error rendering.

src/web/js/output-ui.js
@@ -777,11 +777,11 @@
             {line: cmloc.start.line, ch: 0},
             {line: cmloc.end.line, ch: endch});
         } else {
-          source = sessionStorage.getItem(cmloc.source)
-            .split("\n")
-            .slice(featured.start.line, featured.end.line + 1)
-            .join("\n");
-          endch = cmSnippet.getLine(cmSnippet.lastLine()).length;
+          var sourceArr = sessionStorage.getItem(cmloc.source)
+              .split("\n")
+              .slice(featured.start.line, featured.end.line + 1);
+          source = sourceArr.join("\n");
+          endch = sourceArr[sourceArr.length - 1].length;
         }
 
         var hack = $("<div>").addClass("repl"); // needed for proper CSS styling of the CM below