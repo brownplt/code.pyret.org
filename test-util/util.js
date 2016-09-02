aa6f33e150d78b7be880e2f23c45ac0e20864e77
Joe Gibbs Politz <joe.politz@gmail.com>
2016-09-02 16:08:50 -0400

    Revert "turn off sauce"
    This reverts commit b2d3eca48b9e5cd5195cdf70165b6a1dbd0231b9.

--- test-util/util.js
+++ test-util/util.js
@@ -15,13 +15,43 @@ function teardownMulti() {
 
 function setupWithName(name) {
   if(this.currentTest) { name = this.currentTest.title; }
-
-  var browser = process.env.TEST_BROWSER || "phantomjs";
-  this.base = process.env.BASE_URL;
-  this.browser = new webdriver.Builder()
-  .withCapabilities({
-    browserName: browser
-  }).build();
+  var browser = process.env.SAUCE_BROWSER || "phantomjs";
+  if (process.env.TRAVIS_JOB_NUMBER != undefined) {
+    this.base = process.env.SAUCE_TEST_TARGET;
+    this.browser = new webdriver.Builder()
+    .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+    .withCapabilities({
+      name: name,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      build: process.env.TRAVIS_BUILD_NUMBER,
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      tags: [process.env.TRAVIS_BRANCH, browser, "travis"],
+      customData: {
+        "browser": browser,
+        "commit": process.env.TRAVIS_COMMIT,
+        "commit-range": process.env.TRAVIS_COMMIT_RANGE,
+        "branch": process.env.TRAVIS_BRANCH,
+      },
+      browserName: browser
+    }).build();
+  } else if(process.env.SAUCE_USERNAME !== undefined) {
+    this.base = process.env.SAUCE_TEST_TARGET;
+    this.browser = new webdriver.Builder()
+    .usingServer('https://ondemand.saucelabs.com/wd/hub')
+    .withCapabilities({
+      name: this.currentTest.title,
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      browserName: browser
+    }).build();
+  } else {
+    this.base = process.env.BASE_URL;
+    this.browser = new webdriver.Builder()
+    .withCapabilities({
+      browserName: browser
+    }).build();
+  }
 
   this.browser.manage().window().maximize();
 
