b1dd1e471642a6ac43dd304412465c7f4bf26ccc
Philip Blair <peblairman@gmail.com>
2016-09-17 12:25:00 -0400

    Fixes clipboard icon on tables (rendered as a white square on my machine!). Also adds padding to last column to better fit the icon.

--- src/web/js/output-ui.js
+++ src/web/js/output-ui.js
@@ -1366,10 +1366,10 @@
           }
         } else if (runtime.ffi.isVSTable(val)) {
           var showText = document.createElement("a");
-          $(showText).text("\uD83D\uDCCB");
+          $(showText).html("<i class=\"fa fa-clipboard\" aria-hidden=\"true\"></i>");
           $(showText).css({
-            'border': '1px solid black',
-            'background': 'white'
+            'margin-top': '0.3em',
+            'margin-right': '0.3em'
           });
           $(showText).addClass("info-icon-top");
           var textDiv = $("<div>").css({"z-index": 15000});
