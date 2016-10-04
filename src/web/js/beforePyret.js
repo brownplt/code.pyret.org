80c73729bbefee55a729d9348174b8351451ad5d
Dorai Sitaram <ds26gte@yahoo.com>
2016-10-04 16:39:18 -0400

    ...

--- src/web/js/beforePyret.js
+++ src/web/js/beforePyret.js
@@ -377,6 +377,10 @@ $(function() {
   programLoaded.fin(function() {
     var pyretLoad = document.createElement('script');
     console.log('process.env is', JSON.stringify(process.env));
+    console.log('process.env.GOOGLE_CLIENT_ID is', process.env.GOOGLE_CLIENT_ID);
+    console.log('process.env.BASE_URL is', process.env.BASE_URL);
+    console.log('process.env.PYRET_RELEASE_BASE is', process.env.PYRET_RELEASE_BASE);
+    console.log('process.env.PYRET is', process.env.PYRET);
     console.log('process.env.PYRET is', process.env.PYRET);
     console.log(process.env.PYRET);
     pyretLoad.src = process.env.PYRET;
