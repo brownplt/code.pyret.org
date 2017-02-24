232db8af2b023446f1146c901696372d738181e0
Joe Gibbs Politz <joe.politz@gmail.com>
2017-02-24 12:07:49 -0800

    try lowercasing index import, which may have been running afoul of case insensitivity on OSX

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import StudentDashboard from './StudentDashboard';
-import '../css/Index.css';
+import '../css/index.css';
 
 ReactDOM.render(
   <StudentDashboard />,
