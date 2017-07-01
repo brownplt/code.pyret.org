8202558c167fffc3958b244c6a3deb9dbe1159b5
Joe Gibbs Politz <joe.politz@gmail.com>
2017-07-01 07:30:17 -0700

    new publish semantics

--- src/web/js/share.js
+++ src/web/js/share.js
@@ -59,8 +59,66 @@ window.makeShareAPI = function makeShareAPI(pyretVersion) {
   }
 
   function showShares(container, originalFile) {
-    container.empty();
+    var shareDiv = $("<div>");
+    shareDiv.dialog({
+      title: "Publish this file",
+      modal: true,
+      overlay : { opacity: 0.5, background: 'black'},
+      width : "70%",
+      height : "auto",
+      closeOnEscape : true
+    });
     var shares = originalFile.getShares();
+    var displayDone = shares.then(function(sharedInstances) {
+      if(sharedInstances.length === 0) {
+        var words = $("<p>").text("This program has not been shared before.  Publishing it by clicking below will make a new copy of the file that you can share with anyone you like.  They will be able tto see your code and run your program.");
+        var createNew = $("<button>").addClass("blueButton").text("Publish it!");
+        shareDiv.append(words);
+        shareDiv.append(createNew);
+        createNew.click(function() {
+          var copy = originalFile.makeShareCopy();
+          copy.fail(function(err) {
+            window.flashError("Couldn't copy the file for sharing.");
+            //showShares(container, originalFile);
+          });
+          copy.then(function(f) {
+            var shareUrl = makeShareUrl(f.getUniqueId());
+            var box = autoHighlightBox(shareUrl);
+            shareDiv.append($("<p>").text("Copy the link below to share it!  Close this window whenever you're done."));
+            shareDiv.append(box);
+            box.focus();
+          });
+        });
+      }
+      else { // has been shared before
+        var words = $("<p>").text("This program has been shared before.  You can copy the link below and share it with anyone you like.  You can also re-publish it, which will copy the current content over the previously published copy.");
+        var createNew = $("<button>").addClass("blueButton").text("Publish it!");
+        var shareUrl = makeShareUrl(sharedInstances[0].getUniqueId());
+        var box = autoHighlightBox(shareUrl);
+        shareDiv.append(words);
+        shareDiv.append(createNew);
+        shareDiv.append(box);
+        createNew.click(function() {
+          originalFile.getContents().then(function(contents) {
+            var saved = sharedInstances[0].save(contents, false);
+            saved.fail(function(err) {
+              window.flashError("Couldn't publish the file.");
+              //showShares(container, originalFile);
+            });
+            saved.then(function(f) {
+              window.flashMessage("Published program updated.")
+            });
+          })
+          .fail(function() {
+            window.flashError("Couldn't get the file contents for publishing");
+          });
+        });
+      }
+    });
+
+
+
+    container.empty();
     container.text("Loading share info...");
     var displayDone = shares.then(function(sharedInstances) {
       container.empty();
