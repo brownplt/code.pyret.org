8d737efcea6276b537c9e49e7d3e9bcde0683045
Joe Gibbs Politz <joe.politz@gmail.com>
2017-08-05 15:59:50 -0400

    Fully standalone-ize CPO
    Why:
      - More consistent build process across pyret-lang and CPO
      - Make dependencies explicit in generated blobs to split "Pyret-stuff" from
        "CPO-stuff
      - Save 5-8 seconds of build time
      - Avoid yucky monkey-patch-like code in the index of the page
    
    What:
      - Give each require-shaped module a unique name
      - Explicitly list npm package dependencies in src/scripts/npm-dependencies
      - Use browserify to aggregate these into the standalone

---
+++ src/scripts/npm-dependencies.js
@@ -0,0 +1,44 @@
+//
+// When building a web-standalone, browserify will parse this file
+// and produce a version which include each dependency that is required()
+//
+sexpr = require("s-expression");
+define("s-expression", [], function() {return sexpr;});
+
+//q = require("q");
+if(!!Q) {
+  define("q", [], function() {return Q;});
+}
+
+seedrandom = require("seedrandom");
+define("seedrandom", [], function() {return seedrandom;});
+
+sourcemap = require("source-map");
+define("source-map", [], function () { return sourcemap; });
+
+jssha256 = require("js-sha256");
+define("js-sha256", [], function () { return jssha256; });
+
+jsmd5 = require("js-md5");
+define("js-md5", [], function () { return jsmd5; });
+
+colorspaces = require("colorspaces");
+define("colorspaces", [], function () { return colorspaces; });
+
+d3 = require("d3");
+define("d3", [], function() { return d3; });
+
+d3_tip = require("d3-tip");
+define("d3-tip", [], function() { return d3_tip; });
+
+
+define("fs", [], function () { return {}; });
+
+define("path", [], function () { return {}; });
+
+define("http", [], function () {return {};});
+
+define("lockfile", [], function () { return {}; });
+
+define("websocket", [], function () { return {}; });
+
