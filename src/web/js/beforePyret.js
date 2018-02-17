b316be58f3274f7d1c305ac894a700fbbf4de36f
Joe Gibbs Politz <joe.politz@gmail.com>
2018-02-17 12:34:06 -0800

    add logging for ids of published programs

--- src/web/js/beforePyret.js
+++ src/web/js/beforePyret.js
@@ -280,6 +280,10 @@ $(function() {
       programLoad.then(function(p) { showShareContainer(p); });
     }
     if(params["get"] && params["get"]["share"]) {
+      logger.log('shared-program-load',
+        {
+          id: params["get"]["share"]
+        });
       programLoad = api.getSharedFileById(params["get"]["share"]);
     }
     if(programLoad) {
