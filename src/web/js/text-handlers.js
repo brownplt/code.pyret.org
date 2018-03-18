26139b6402b4d944046854a803293e6a2cc53020
Seth Poulsen <seth@caffeine.local>
2018-03-18 00:01:44 -0600

    Autocorrect en dash on paste (issue #155)

--- src/web/js/text-handlers.js
+++ src/web/js/text-handlers.js
@@ -6,8 +6,28 @@
   provides: {},
   theModule: function(runtime, _, uri) {
 
+    function autoCorrect(instance, changeObj, cm){
+      $('.notificationArea .autoCorrect').remove();
+      curlyQuotes(instance, changeObj, cm);
+      enDash(instance, changeObj, cm);
+    }
+
+    function enDash(instance, changeObj, cm){
+      endashbool = false;
+      if((changeObj.origin == "paste")){
+          var newText = jQuery.map(changeObj.text, function(str, i) {
+            endashbool = endashbool || (str.search(/[\u2013]/g) > -1);
+            str = str.replace(/\u2013/g, "-")
+            return str;
+          });
+        if(endashbool){
+          autoCorrectUndo("Invalid Dash (en dash) converted", changeObj.text, changeObj.from, cm);
+          changeObj.update(undefined, undefined, newText);
+        }
+      }
+    }
+
     function curlyQuotes(instance, changeObj, cm){
-      $('.notificationArea .curlyQ').remove();
       curlybool = false;
       if((changeObj.origin == "paste")){
           var newText = jQuery.map(changeObj.text, function(str, i) {
@@ -19,31 +39,32 @@
             return str;
           });
         if(curlybool){
-          curlyQUndo(changeObj.text, changeObj.from, cm);
+          autoCorrectUndo("Curly quotes converted", changeObj.text, changeObj.from, cm);
           changeObj.update(undefined, undefined, newText);
         }
       }
     }
 
-    function curlyQUndo(oldText, from, cm){
+    function autoCorrectUndo(message, oldText, from, cm){
       var lineN = oldText.length - 1
       var to = {line: from.line + lineN, ch: from.ch + oldText[lineN].length}
       console.log(from, to);
-      message = "Curly quotes converted"
-      var container = $('<div>').addClass("curlyQ")
-      var msg = $("<span>").addClass("curlyQ-msg").text(message);
-      var button = $("<span>").addClass("curlyQ-button").text("Click to Undo");
+      var container = $('<div>').addClass("autoCorrect")
+      var msg = $("<span>").addClass("autoCorrect-msg").text(message);
+      var button = $("<span>").addClass("autoCorrect-button").text("Click to Undo");
       container.append(msg).append(button);
       container.click(function(){
         cm.replaceRange(oldText, from, to);
+        console.log("oldtext, from, to", oldText)
+        console.log(from, to);
       });
       $(".notificationArea").prepend(container);
       container.delay(15000).fadeOut(3000);
     }
 
     return runtime.makeJSModuleReturn({
-      curlyQuotes: curlyQuotes
-      
+      autoCorrect: autoCorrect
+
     });
   }
 })
