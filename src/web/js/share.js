812f259d43716e655a5cd01d3bcbb7abf18bc4ed
Dorai Sitaram <ds26gte@yahoo.com>
2018-04-11 23:18:52 -0400

    Added aria-label for Publish button.

--- src/web/js/share.js
+++ src/web/js/share.js
@@ -50,7 +50,7 @@ window.makeShareAPI = function makeShareAPI(pyretVersion) {
   $(".menuButton a").click(hideAllHovers);
 
   function makeShareLink(originalFile) {
-    var link = $("<div>").append($("<button class=blueButton>").text("Publish"));
+    var link = $("<div>").append($('<button aria-label="Publish, F9" class=blueButton>').text("Publish"));
     var shareDiv = $("<div>").addClass("share");
     link.click(function() { showShares(shareDiv, originalFile); });
     return link;
