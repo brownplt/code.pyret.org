32370c536b78c9ade4d6dda521fc452df6d53033
Joe Gibbs Politz <joe.politz@gmail.com>
2019-03-22 17:36:47 -0700

    add color-at-position to get single colors while avoiding list/array conversion

--- src/web/js/trove/image-lib.js
+++ src/web/js/trove/image-lib.js
@@ -1607,6 +1607,25 @@
 
     LineImage.prototype = heir(BaseImage.prototype);
 
+    var colorAtPosition = function(img, x, y) {
+      var width = img.getWidth(),
+      height = img.getHeight(),
+      canvas = makeCanvas(width, height),
+      ctx = canvas.getContext("2d"),
+      r, g, b, a;
+      img.render(ctx, 0, 0);
+      imageData = ctx.getImageData(0, 0, width, height);
+      data = imageData.data,
+      index = (y * width + x) * 4;
+
+      r = data[index]
+      g = data[index + 1];
+      b = data[index + 2];
+      a = data[index + 3];
+
+      return makeColor(r, g, b, a);
+    }
+
     var imageToColorList = function(img) {
       var width = img.getWidth(),
       height = img.getHeight(),
@@ -1792,6 +1811,7 @@
       makeFileImage: makeFileImage,
       makeVideoImage: makeFileVideo,
 
+      colorAtPosition: colorAtPosition,
       imageToColorList: imageToColorList,
       colorListToImage: colorListToImage,
 
