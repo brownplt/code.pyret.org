250b24a6ae89805440dd545fd8b65e2a8ccb7c6b
Ben Lerner <blerner@ccs.neu.edu>
2020-08-11 09:08:31 -0400

    add BS:DS share links, and generalize the timeout mechanism slightly in case there's a long-running program

--- test/shareUrls.js
+++ test/shareUrls.js
@@ -2,12 +2,26 @@ var assert = require("assert");
 var tester = require("../test-util/util.js");
 
 const shares = [
-  // name : An informative title for the test case output
   // id : /editor#share=<id>
+  // name : An informative title for the test case output
   // expr : Run at REPL after running definitions and expected to *not produce an error*
   // modal : How many modal windows to expect to have to close
-  { name: "Data Science 1.5", id: "1Z8ncVGKqWiED_wHl8TlNF3D9AGBb7awm", expr: "animals-table.column('name')", modal: 0 },
-  { name: "Blank Game.arr", id: "1xL3ZnWb43d5ih_fRib3dz3h8z9d__2om", expr: "BACKGROUND", modal: 1 }
+  // timeout : optionally a timeout to override the default, if it's a long-running program
+  { id: "1Z8ncVGKqWiED_wHl8TlNF3D9AGBb7awm", name: "Data Science 1.5",
+    expr: "animals-table.column('name')", modal: 0 },
+  { id: "1xL3ZnWb43d5ih_fRib3dz3h8z9d__2om", name: "Blank Game.arr",
+    expr: "BACKGROUND", modal: 1 },
+
+  // BS:DS starter files
+  { id: "1VVz4l0P6GLwbcpYyAGYJuRgBxj69R52Z", name: "Trust, but verify!",
+    expr: "verify1", modal: 0 },
+  { id: "1Fm3bSkeWZ5f4VwZ24TtOkZ3Pu0CPkJT2", name: "Table Methods Starter File (v1.5.1)",
+    expr: "animals-table", modal: 0 },
+  { id: "1ymyvlI7RTtq8lHh4VH3x1N3WlcZB650j", name: "Mood Generator Starter File",
+    expr: "mood('happy')", modal: 0 },
+  { id: "1d3HuG_LjdX9HpfQPCmVmDG9mYml4nOX8", name: "Random Animals Starter File (v1.5.1)",
+    expr: "big-animals-table", modal: 0 },
+
 ];
 
 describe("Load share urls for known starter files", function() {
@@ -15,14 +29,15 @@ describe("Load share urls for known starter files", function() {
   afterEach(tester.teardown);
 
   shares.forEach(function(share) {
+    var timeout = share.timeout !== undefined ? share.timeout : 30000;
     it("should load the shared file at " + share.id + " (" + share.name + ")", function(done) {
-      this.timeout(20000);
+      this.timeout(timeout);
       var self = this;
       this.browser.get(this.base + "/editor#share=" + share.id);
       this.browser.wait(function() { return tester.pyretLoaded(self.browser); });
       tester.evalDefinitions(self.browser, {});
       for(var i = 0; i < share.modal; i += 1) {
-        tester.waitForWorldProgram(self.browser, 20000, 5000);
+        tester.waitForWorldProgram(self.browser, timeout, 5000);
       }
       tester.waitForBreakButton(this.browser);
       this.browser.wait(function() { return tester.evalPyretNoError(self.browser, share.expr); });
