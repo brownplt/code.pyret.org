f73b4029caf1439236a57ef393d467c692358a7d
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2021-12-02 12:41:57 -0800

    Fix #408: hide legend if there is only one DataSeries

--- src/web/js/trove/chart-lib.js
+++ src/web/js/trove/chart-lib.js
@@ -506,6 +506,7 @@
     const combined = scatters.concat(lines);
     const legends = [];
     let cnt = 1;
+    const legendEnabled = combined.length > 1;
     combined.forEach(p => {
       let legend = get(p, 'legend');
       if (legend === '') {
@@ -571,7 +572,7 @@ ${labelRow}`;
         }
         return seriesOptions;
       }),
-      legend: {position: 'bottom',},
+      legend: {position: legendEnabled ? 'bottom' : 'none'},
       crosshair: {trigger: 'selection'}
     };
 
