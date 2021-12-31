0141088175b1a1e382ceaa22f7f6f1f6328d5932
schornb <55115049+schornb@users.noreply.github.com>
2021-12-31 13:21:40 -0500

    changed geochart-from-list annotations

--- make-template.js
+++ make-template.js
@@ -1,8 +1,8 @@
-var mustache = require('mustache');
-var file = require('fs');
+var mustache = require("mustache");
+var file = require("fs");
 // Silent suppresses "missing .env file" warning,
 // which we want since deploys don't have that file
-require('dotenv').config({ silent: true });
+require("dotenv").config({ silent: true });
 
 var config = process.env;
 
@@ -10,4 +10,3 @@ var fileIn = process.argv[2];
 var fileContents = String(file.readFileSync(fileIn));
 
 process.stdout.write(mustache.render(fileContents, config));
-
