6e4a7e3730710fdad55ee3792fd991caea9ddc15
John UUU <john_w_wu@brown.edu>
2022-01-01 22:15:07 -0600

    Changed multi-bar-chart.colors behavior when less colors are given than number of components
    1) Before when a multi-bar-chart series w/ n components called the **colors** method with m colors, where m < n, the rest of the components would all be colored black. This commit updates the code to default back to google chart default colors (Hard-Coded) instead!

--- src/web/js/trove/chart-lib.js
+++ src/web/js/trove/chart-lib.js
@@ -586,7 +586,15 @@
     const pointer_color = get_pointer_color(rawData);
     const axis = get_axis(rawData);
     const interval_color = get_interval_color(rawData); 
+    const default_colors = ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099',
+                            '#3B3EAC', '#0099C6', '#DD4477', '#66AA00', '#B82E2E',
+                            '#316395', '#994499', '#22AA99', '#AAAA11', '#6633CC',
+                            '#E67300', '#8B0707', '#329262', '#5574A6', '#3B3EAC']
     var colors_list = get_colors_list(rawData);
+    if (colors_list.length < default_colors.length) {
+      default_colors.splice(0, colors_list.length, ...colors_list);
+      colors_list = default_colors;
+    }
 
     // Initializes the Columns of the data 
     data.addColumn('string', 'Label');
