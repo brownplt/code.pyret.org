0944f086d67e5d2eadc274df470bedce4b462c89
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-06-05 09:28:39 -0400

    ?

--- src/scripts/npm-dependencies.js
+++ src/scripts/npm-dependencies.js
@@ -75,7 +75,10 @@ define("lockfile", [], function () { return {}; });
 
 define("websocket", [], function () { return {}; });
 
-// [SP]: TOD): This is where we would pull in an npm package.
+// [SP]: TODO: This is where we would pull in an npm package.
 // So publish the npm package w/ the standalone JS for cnd-lib.
 // define("cnd-lib", [], function () { return {}; });
 // Then this is importable as a native dependency in dom-render.js
+
+cndjs = require("cnd-js");
+define("cnd-js", [], function () { return cndjs; });
--- src/web/js/trove/dom-render.js
+++ src/web/js/trove/dom-render.js
@@ -1,12 +1,13 @@
 ({
     requires: [],
-    nativeRequires: [],
+    nativeRequires: ["cnd-js"],
     provides: {
         values: {
-            styled: ["arrow", [["RawArray", "Any"], "String"], "Any"]
+            styled: ["arrow", [["RawArray", "Any"], "String"], "Any"],
+            layout: ["arrow", [["RawArray", "Any"], "Any"], "Any"] // Ideally this should be an Object of some kind, and then a Promise (or resolution of a Promise).
         }
     },
-    theModule: function(runtime, namespace, uri) {
+    theModule: function(runtime, namespace, uri, cndjs) {
 
         // TODO: Write a variant of this to make 
         // things work.
@@ -19,8 +20,25 @@
             container.style = style;
             return container;
         }
+
+        // TODO: THis asynchronous behavior may not be 
+        // a Pyret pattern, and may break things?
+        async function layout(nodes, spec) {
+
+            // This is a placeholder for where we would actually put together the 
+            // specification.
+            console.log("Layout spec", spec);
+
+
+            let x = await cndjs.arrangeElements(nodes, [],[],[]);
+            console.log("Arranged elements", x);
+            return cndjs.getRenderers("dom")(x);
+        }
+
+
         return runtime.makeModuleReturn({
-            styled: runtime.makeFunction(styled)
+            styled: runtime.makeFunction(styled),
+            layout: runtime.makeFunction(layout)
         }, {});
     }
     
